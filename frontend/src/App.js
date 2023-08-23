5f94f2f38e2434a79a002054f0724ad445eb2fc2
Lars Berger <larserikberger08@gmail.com>
2023-08-23 11:19:35 +0200

    first push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { Container } from "react-bootstrap";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
-  return <h1>Welcome to Shoppaholic!</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
